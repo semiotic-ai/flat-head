d75023743529d805078aec5c9dd920f9f29a3ec5
pedro bufulin <pedro.almeida@ufu.br>
2024-02-21 22:34:09 -0300

    feat: utility to generate flat files names for fetching from buckets,webdavs, ftp

---
+++ src/utils.rs
@@ -0,0 +1,22 @@
+use header_accumulator::utils::MAX_EPOCH_SIZE;
+
+/// return the filenames of files to be fetched, between a starting and an ending epoch
+pub fn gen_dbin_filenames(start: u64, end: u64) -> Vec<String> {
+    let mut filenames = Vec::new();
+
+    // TODO: better error handling
+    if start >= end {
+        panic!("start can't be equal or above end epoch")
+    }
+
+    let start_blocks = start * MAX_EPOCH_SIZE as u64;
+    let end_blocks = end * 8200 as u64;
+    //TODO: count for the FINAL_EPOCH files, which might not be eaxctly 100 blocks named
+
+    for number in (start_blocks..=end_blocks).step_by(100) {
+        let filename = format!("{:010}.dbin.zst", number);
+        filenames.push(filename);
+    }
+
+    filenames
+}
